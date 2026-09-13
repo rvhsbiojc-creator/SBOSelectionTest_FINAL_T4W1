9a53dc64edd43dd338bb809255c9b44ca3dd1552
rvhsbiojc-creator <rvhsbiojc@gmail.com>
2026-09-13 16:30:58 +0800

    Update GAS_WEB_APP_URL for new domain

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 const APP_CONFIG = {
   TEST_TITLE: "SBO Selection Test_T3W1 2026",
-  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxc-VTk7qYofVJ9LqGB9MTy8UsGq4lL8ujlH0nr1cFcvM5DQMZfvk3gAWfCTnyHhTIzuw/exec",
+  GAS_WEB_APP_URL: "https://script.google.com/a/macros/moe.edu.sg/s/AKfycbxc-VTk7qYofVJ9LqGB9MTy8UsGq4lL8ujlH0nr1cFcvM5DQMZfvk3gAWfCTnyHhTIzuw/exec",
   QUESTION_IMAGE_BASE_PATH: "assets/questions/",
   LOCAL_DEMO_MODE: false,
   API_TIMEOUT_MS: 18000,
